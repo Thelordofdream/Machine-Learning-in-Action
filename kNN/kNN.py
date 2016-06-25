2cfe97a144febe1ab97f185871c1ba2940f36a39
Thelordofdream <574483817@qq.com>
2016-06-25 18:44:48 +0800

    upload first time

---
+++ kNN/kNN.py
@@ -0,0 +1,145 @@
+from numpy import *
+import operator
+import matplotlib
+import matplotlib.pyplot as plt
+from os import listdir
+
+'''
+import os
+os.chdir("/Users/zhangmingjie/PycharmProjects/kNN")
+execfile("kNN.py")
+import kNN
+group,labels=kNN.createDataSet()
+kNN.classify0([18,90],group,labels,3)
+datingDataMat,datingLabels=kNN.file2matrix('datingTestSet.txt')
+import matplotlib
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax=fig.add_subplot(111)
+ax.scatter(datingDataMat[:,0],datingDataMat[:,1],15.0*array(datingLabels),15.0*array(datingLabels))
+plt.xlabel('Frequent Flier Miles Earned Per Year')
+plt.ylabel('Percentage of Time Spent Playing Video Games')
+plt.show()
+normMat,ranges,minVals=kNN.autoNorm(datingDataMat)
+kNN.datingClassTest()
+kNN.classifyPerson()
+kNN.handwritingClassTest()
+reload(kNN)
+'''
+
+
+def createDataSet():
+    group = array([[3,104],[2,100],[1,81],[101,10],[99,5],[98,2]])
+    labels = ['Love Movie','Love Movie','Love Movie','Action Movie','Action Movie','Action Movie']
+    return group, labels
+
+
+def classify0(inX,dataSet,labels,k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX,(dataSetSize,1))-dataSet
+    sqDiffMat = diffMat**2
+    sqDistance = sqDiffMat.sum(axis=1)
+    distance = sqDistance**0.5
+    sortedDistIndicies = distance.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] =  classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines =len(arrayOLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelsVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        if listFromLine[-1].isdigit():
+            classLabelsVector.append(int(listFromLine[-1]))
+        else:
+            classLabelsVector.append(str(listFromLine[-1]))
+        index += 1
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    #ax.scatter(returnMat[:,0],returnMat[:,1],15.0*array(classLabelsVector),15.0*array(classLabelsVector))
+    #plt.xlabel('Frequent Flier Miles Earned Per Year')
+    #plt.ylabel('Percentage of Time Spent Playing Video Games')
+    #plt.show()
+    return returnMat,classLabelsVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges =maxVals-minVals
+    normDataSet =zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))
+    return normDataSet,ranges,minVals
+
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],5)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult,datingLabels[i])
+        if (classifierResult != datingLabels[i]):errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
+
+
+def classifyPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per week?"))
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles,percentTats,iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels,5)
+    print "You will probably like this person:", resultList[classifierResult - 1]
+
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m-1):
+        fileNameStr = trainingFileList[i+1]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest-1):
+        fileNameStr = testFileList[i+1]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
+        print "the classsifier came back with: %d, the real answer is: %d" % (classifierResult,classNumStr)
+        if (classifierResult != classNumStr):errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
