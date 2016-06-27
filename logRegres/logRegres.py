c63750277d5ded22cff2cff2686f9053a1bbe960
Thelordofdream <574483817@qq.com>
2016-06-28 01:51:01 +0800

    add Logistic Regression

--- logRegres/logRegres.py
+++ logRegres/logRegres.py
@@ -54,7 +54,7 @@ def plotBestFit(weights):
     ax.scatter(xcord2, ycord2, s=30, c='green')
     x = arange(-3.0, 3.0, 0.1)
     y = (-weights[0] - weights[1] * x) / weights[2]
-    ax.plot(x, y.tolist()[0])
+    ax.plot(x, y)
     plt.xlabel('X1')
     plt.ylabel('X2')
     plt.show()
@@ -62,4 +62,4 @@ def plotBestFit(weights):
 
 dataMatIn, classLabels = loadDataSet()
 weights = gradAscent(dataMatIn, classLabels)
-plotBestFit(weights)
+plotBestFit(weights.getA())
